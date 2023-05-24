05434baa36d368bca720cbfcb6ead471ec085e97
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2023-05-23 21:05:35 -0300

    refactor: reimport function

--- src/compressor/compressor.service.ts
+++ src/compressor/compressor.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
-import * as compressImages from 'compress-images';
+import compressImages from 'compress-images';
 import * as fs from 'fs';
 import {
   IMAGE_COMPRESSED_NAME,
